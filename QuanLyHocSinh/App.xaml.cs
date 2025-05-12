f66faadfdf475c58a6464269aff75b6a8a98c914
c1khoa <anhkhoa10082005@gmail.com>
2025-05-13 05:39:39 +0700

    Initial commit

---
+++ QuanLyHocSinh/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using QuanLyHocSinh.ViewModel;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace QuanLyHocSinh;
+
+/// <summary>  
+/// Interaction logic for App.xaml  
+/// </summary>  
+public partial class App : Application
+{
+}
---
+++ QuanLyHocSinh/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using QuanLyHocSinh.View.Windows;
+
+
+namespace QuanLyHocSinh.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool Isloaded { get; set; } = false;
+
+        public ICommand LoadedWindowCommand { get; set; }
+        // Mọi thứ xử lý nằm trong này
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>(
+                (p) => { return true; },
+                (p) =>
+                {
+                    if (Isloaded) return;
+                    Isloaded = true;
+                    LoginWindow login = new LoginWindow();
+                    login.ShowDialog();
+                }
+            );
+            // Đoạn này để xử lý khi nhấn nút thoát
+            Application.Current.MainWindow.Closing += (s, e) =>
+            {
+                if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            };
+
+
+        }
+    }
+}
+
