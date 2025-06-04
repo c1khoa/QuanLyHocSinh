bbd2633c78a055dec4cb21d2dbd71abfcbe7c522
NickKurriyama <157307965+NickKurriyama@users.noreply.github.com>
2025-06-04 22:24:32 +0700

    Update BaseViewModel.cs

--- QuanLyHocSinh/ViewModel/BaseViewModel.cs
+++ QuanLyHocSinh/ViewModel/BaseViewModel.cs
@@ -1,3 +1,4 @@
+using QuanLyHocSinh.Model.Entities;
 using QuanLyHocSinh.View.Controls.QuanLyTaiKhoan;
 using System;
 using System.Collections.Generic;
@@ -9,27 +10,22 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace QuanLyHocSinh.ViewModel
-{
-    public class BaseViewModel : INotifyPropertyChanged
+{    public class BaseViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public static implicit operator BaseViewModel(QuanLyTaiKhoanThemUC v)
-        {
-            throw new NotImplementedException();
-        }
     }
-    class RelayCommand<T> : ICommand
+    public class RelayCommand<T> : ICommand
     {
-        private readonly Predicate<T> _canExecute;
+        private readonly Predicate<T>? _canExecute;
         private readonly Action<T> _execute;
 
-        public RelayCommand(Predicate<T> canExecute, Action<T> execute)
+        public RelayCommand(Predicate<T>? canExecute, Action<T> execute)
         {
             if (execute == null)
                 throw new ArgumentNullException("execute");
@@ -37,11 +33,13 @@ namespace QuanLyHocSinh.ViewModel
             _execute = execute;
         }
 
-        public bool CanExecute(object parameter)
+        public RelayCommand(Action<T> execute) : this(null, execute) { }
+
+        public bool CanExecute(object? parameter)
         {
             try
             {
-                return _canExecute == null ? true : _canExecute((T)parameter);
+                return _canExecute == null ? true : _canExecute((T)parameter!);
             }
             catch
             {
@@ -49,12 +47,39 @@ namespace QuanLyHocSinh.ViewModel
             }
         }
 
-        public void Execute(object parameter)
+        public void Execute(object? parameter)
         {
-            _execute((T)parameter);
+            _execute((T)parameter!);
         }
 
-        public event EventHandler CanExecuteChanged
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }    // RelayCommand không generic
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler? CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
