d0f22b0b004db11bcc76943a931bc3dffc5c3ce5
NickKurriyama <157307965+NickKurriyama@users.noreply.github.com>
2025-06-04 22:26:15 +0700

    Update UserService.cs

--- QuanLyHocSinh/Services/UserService.cs
+++ QuanLyHocSinh/Services/UserService.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using QuanLyHocSinh.Model.Entities;
+using System.Collections.Generic;
 
-namespace QuanLyHocSinh.Services
+namespace QuanLyHocSinh.Service
 {
-    public class UserService
+    public static class UserService
     {
+        private static List<User> _users = new List<User>();
+
+        public static List<User> LayDanhSachTaiKhoan()
+        {
+            return _users;
+        }
+
+        public static bool ThemTaiKhoan(User user)
+        {
+            if (_users.Exists(u => u.TenDangNhap == user.TenDangNhap))
+            {
+                return false; // Tên đăng nhập đã tồn tại
+            }
+
+            _users.Add(user);
+            return true;
+        }
     }
 }
