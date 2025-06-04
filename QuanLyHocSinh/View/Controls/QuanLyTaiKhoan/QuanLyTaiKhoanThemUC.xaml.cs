3ff4e852374723593956f09914c039ad73b4820a
NickKurriyama <157307965+NickKurriyama@users.noreply.github.com>
2025-06-04 22:23:14 +0700

    Update QuanLyTaiKhoanThemUC.xaml.cs

--- QuanLyHocSinh/View/Controls/QuanLyTaiKhoan/QuanLyTaiKhoanThemUC.xaml.cs
+++ QuanLyHocSinh/View/Controls/QuanLyTaiKhoan/QuanLyTaiKhoanThemUC.xaml.cs
@@ -1,28 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using QuanLyHocSinh.ViewModel;
+using QuanLyHocSinh.ViewModel.QuanLyTaiKhoan;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace QuanLyHocSinh.View.Controls.QuanLyTaiKhoan
 {
-    /// <summary>
-    /// Interaction logic for QuanLyTaiKhoan_ThemUC.xaml
-    /// </summary>
     public partial class QuanLyTaiKhoanThemUC : UserControl
     {
-        public QuanLyTaiKhoanThemUC()
+        private readonly QuanLyTaiKhoanThemViewModel _viewModel;
+
+        public QuanLyTaiKhoanThemUC(QuanLyTaiKhoanThemViewModel viewModel)
         {
             InitializeComponent();
+            _viewModel = viewModel;
+            DataContext = _viewModel;
+
+            _viewModel.CancelRequested += () =>
+            {
+                var mainVM = Application.Current.MainWindow?.DataContext as MainViewModel;
+                mainVM?.NavigateBack();
+            };
+
+            _viewModel.AccountAddedSuccessfully += user =>
+            {
+                var mainVM = Application.Current.MainWindow?.DataContext as MainViewModel;
+                mainVM?.NavigateBack();
+            };
+        }
+
+        private void btnShowHidePassword_Checked(object sender, RoutedEventArgs e)
+        {
+            txtUserPassWordVisible.Text = txtUserPassWord.Password;
+            txtUserPassWord.Visibility = Visibility.Collapsed;
+            txtUserPassWordVisible.Visibility = Visibility.Visible;
+        }
+
+        private void btnShowHidePassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            txtUserPassWord.Password = txtUserPassWordVisible.Text;
+            txtUserPassWordVisible.Visibility = Visibility.Collapsed;
+            txtUserPassWord.Visibility = Visibility.Visible;
+        }
+
+        private void txtUserPassWordVisible_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_viewModel != null)
+            {
+                _viewModel.MatKhau = txtUserPassWordVisible.Text;
+            }
+        }
+
+        private void txtUserPassWord_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (_viewModel != null)
+            {
+                _viewModel.MatKhau = txtUserPassWord.Password;
+            }
         }
     }
 }
