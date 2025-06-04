3e3e37dfc8f2e595513aa80b360ed9df02a73ded
NickKurriyama <157307965+NickKurriyama@users.noreply.github.com>
2025-06-04 22:22:28 +0700

    Update QuanLyTaiKhoanMainUC.xaml.cs

--- QuanLyHocSinh/View/Controls/QuanLyTaiKhoan/QuanLyTaiKhoanMainUC.xaml.cs
+++ QuanLyHocSinh/View/Controls/QuanLyTaiKhoan/QuanLyTaiKhoanMainUC.xaml.cs
@@ -1,28 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using QuanLyHocSinh.ViewModel;
+using QuanLyHocSinh.ViewModel.QuanLyTaiKhoan;
+using System;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace QuanLyHocSinh.View.Controls.QuanLyTaiKhoan    
+namespace QuanLyHocSinh.View.Controls.QuanLyTaiKhoan
 {
-    /// <summary>
-    /// Interaction logic for QuanLyTaiKhoanUC.xaml
-    /// </summary>
     public partial class QuanLyTaiKhoanMainUC : UserControl
     {
-        public QuanLyTaiKhoanMainUC()
+        public QuanLyTaiKhoanMainUC(MainViewModel mainVM)
         {
             InitializeComponent();
+            DataContext = new QuanLyTaiKhoanMainViewModel(mainVM);
         }
     }
 }
