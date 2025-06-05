49cb36f4aeed7c40061109d71e9521d26a491014
NickKurriyama <157307965+NickKurriyama@users.noreply.github.com>
2025-06-05 11:42:07 +0700

    Update QuanLyTaiKhoanMainViewModel.cs

--- QuanLyHocSinh/ViewModel/QuanLyTaiKhoan/QuanLyTaiKhoanMainViewModel.cs
+++ QuanLyHocSinh/ViewModel/QuanLyTaiKhoan/QuanLyTaiKhoanMainViewModel.cs
@@ -1,5 +1,6 @@
 using QuanLyHocSinh.Model.Entities;
 using QuanLyHocSinh.Service;
+using System;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
@@ -8,74 +9,62 @@ namespace QuanLyHocSinh.ViewModel.QuanLyTaiKhoan
 {
     public class QuanLyTaiKhoanMainViewModel : BaseViewModel
     {
-        private readonly MainViewModel _mainVM;
+        public readonly MainViewModel _mainVM;
 
-        public ObservableCollection<User> DanhSachTaiKhoan { get; }
+        public ObservableCollection<User> Users { get; } = new ObservableCollection<User>();
         public ICommand ShowThemTaiKhoanCommand { get; }
         public ICommand ReloadCommand { get; }
 
         public QuanLyTaiKhoanMainViewModel(MainViewModel mainVM)
         {
             _mainVM = mainVM ?? throw new ArgumentNullException(nameof(mainVM));
-            DanhSachTaiKhoan = new ObservableCollection<User>();
 
             LoadDanhSachTaiKhoan();
 
-            ShowThemTaiKhoanCommand = new RelayCommand(
-                execute: () => ShowThemTaiKhoan()
-            );
-
-            ReloadCommand = new RelayCommand(
-                execute: () => LoadDanhSachTaiKhoan()
-            );
+            ShowThemTaiKhoanCommand = new RelayCommand(ShowThemTaiKhoan);
+            ReloadCommand = new RelayCommand(LoadDanhSachTaiKhoan);
         }
 
-        private void ShowThemTaiKhoan()
+        public void ShowThemTaiKhoan()
+        {
+            // Trong phương thức ShowThemTaiKhoan()
+            var themVM = new QuanLyTaiKhoanThemViewModel(_mainVM);
+
+            themVM.AccountAddedSuccessfully += (userMoi) =>
+            {
+                Users.Insert(0, userMoi);
+                _mainVM.CurrentView = this; // Quay lại view chính
+            };
+
+            themVM.CancelRequested += () =>
+            {
+                _mainVM.CurrentView = this; // Quay lại view chính
+            };
+
+            _mainVM.CurrentView = themVM;
+        }
+
+        public void LoadDanhSachTaiKhoan()
         {
             try
             {
-                // Sửa: Truyền mainVM vào constructor
-                var themVM = new QuanLyTaiKhoanThemViewModel(_mainVM);
-
-                themVM.AccountAddedSuccessfully += (userMoi) =>
-                {
-                    Application.Current.Dispatcher.Invoke(() =>
-                    {
-                        DanhSachTaiKhoan.Insert(0, userMoi);
-                    });
-                };
-
-                // Thêm xử lý khi hủy bỏ
-                themVM.CancelRequested += () =>
-                {
-                    _mainVM.CurrentView = this; // Quay lại view hiện tại
-                };
-
-                _mainVM.CurrentView = themVM;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Lỗi khi mở form thêm tài khoản: {ex.Message}");
-            }
-        }
-
-        private void LoadDanhSachTaiKhoan()
-        {
-            try
-            {
-                DanhSachTaiKhoan.Clear();
+                Users.Clear();
                 var danhSach = UserService.LayDanhSachTaiKhoan();
 
-                if (danhSach != null)
+                if (danhSach == null)
                 {
-                    foreach (var user in danhSach)
-                    {
-                        if (user != null)
-                        {
-                            DanhSachTaiKhoan.Add(user);
-                        }
-                    }
+                    MessageBox.Show("Không thể tải danh sách tài khoản (null).");
+                    return;
                 }
+
+                foreach (var user in danhSach)
+                {
+                    if (user == null) continue;
+
+                    user.VaiTro ??= new VaiTro();
+                    Users.Add(user);
+                }
+
             }
             catch (Exception ex)
             {
