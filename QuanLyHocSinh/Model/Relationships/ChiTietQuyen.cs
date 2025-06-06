f5b5098681b7472d557bf8b9c55f602d7dec217d
Nick Kurriyama <nickkurriyama@gmail.com>
2025-06-06 21:03:12 +0700

    Cập nhật code từ QuanLyHocSinh-main vào nhánh quydinh

--- QuanLyHocSinh/Model/Relationships/ChiTietQuyen.cs
+++ QuanLyHocSinh/Model/Relationships/ChiTietQuyen.cs
@@ -8,5 +8,12 @@ namespace QuanLyHocSinh.Model.Relationships
 {
     class ChiTietQuyen
     {
+        public string ChiTietQuyenID { get; set; } = string.Empty;
+
+        public string QuyenID { get; set; } = string.Empty;
+
+        public string VaiTroID { get; set; } = string.Empty;
+
+        public string TuongTac { get; set; } = string.Empty;
     }
 }
--- QuanLyHocSinh/Model/Relationships/PhanQuyen.cs
+++ QuanLyHocSinh/Model/Relationships/PhanQuyen.cs
@@ -8,5 +8,14 @@ namespace QuanLyHocSinh.Model.Relationships
 {
     class PhanQuyen
     {
+        public string PhanQuyenID { get; set; } = string.Empty;
+
+        public string QuyenID { get; set; } = string.Empty;
+
+        public string GiaoVuPhanQuyenID { get; set; } = string.Empty;
+
+        public string UserDuocPhanQuyenID { get; set; } = string.Empty;
+
+        public DateTime NgayPhanQuyen { get; set; }
     }
 }
