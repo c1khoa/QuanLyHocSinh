9ff59253c05df9c5b66fdbd1d3637a15812c3a65
c1khoa <anhkhoa10082005@gmail.com>
2025-06-06 21:02:25 +0700

    Restore MonHoc.cs and NamHoc.cs from tuananh

---
+++ QuanLyHocSinh/Model/Entities/NamHoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyHocSinh.Model.Entities
+{
+    public class NamHoc
+    {
+        public string NamHocID { get; set; }
+        public string MoTa { get; set; }
+        public DateTime BatDau { get; set; }
+        public DateTime KetThuc { get; set; }
+    }
+}
