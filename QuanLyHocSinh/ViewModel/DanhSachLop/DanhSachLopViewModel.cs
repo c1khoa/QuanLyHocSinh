3caa6c8340310c2d0b5a2cc13bb4c47424913ed7
c1khoa <anhkhoa10082005@gmail.com>
2025-07-01 02:44:42 +0700

    ds lop

---
+++ QuanLyHocSinh/ViewModel/DanhSachLop/DanhSachLopViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyHocSinh.ViewModel.DanhSachLop
+{
+
+    public class DanhSachLopViewModel : BaseViewModel
+    {
+        private readonly MainViewModel _mainVM;
+        public DanhSachLopViewModel(MainViewModel mainVM)
+        {
+            _mainVM = mainVM ?? throw new ArgumentNullException(nameof(mainVM));
+            // Initialize commands and load data here
+        }
+    }
+}
