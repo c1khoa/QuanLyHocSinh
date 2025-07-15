29b932d5348d49f2cea814c39796cd61ee5e2438
Nick Kurriyama <nickkurriyama@gmail.com>
2025-07-15 12:26:20 +0700

    Push project to testcase branch

---
+++ QuanLyHocSinh/TestProject/TestTarget/MyCaculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TestTarget
+{
+    public class MyCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Cannot divide by zero.");
+            }
+            return a / b;
+        }
+    }
+
+    public class AuthenticationService
+    {
+        public bool Login(string username, string password)
+        {
+            // Đây là logic đăng nhập giả định
+            return username == "admin" && password == "password";
+        }
+    }
+}
